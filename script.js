d370be2cd677693a04f8be7f13daa174a56be095
irinaravenova <hernandezirina23@gmail.com>
2023-08-22 09:47:28 -0400

    populated and styled dummy value for display window

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function divide(a, b) {
 
 function operate(a, b, operator) {
     if (operator == "+") {
-        add(a, b);
+        return add(a, b);
     }
     if (operator == "-") {
-        subtract(a, b);
+        return subtract(a, b);
     }
     if (operator == "*") {
-        multiply(a, b)
+        return multiply(a, b)
     }
     if (operator == "/") {
-        divide(a, b);
+        return divide(a, b);
     }
 
 
